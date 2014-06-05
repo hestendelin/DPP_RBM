09320cdd3d9c53e4a81319fcc74470422fe182b6
enalisnick <enalisnick@gmail.com>
2014-06-05 16:03:14 -0700

    Create agg_digits.m

---
+++ agg_digits.m
@@ -0,0 +1,60 @@
+numPerClass = 5000;
+mnist = nan(784,numPerClass*10);
+for idx=1:10
+    load(['digit' num2str(idx-1) '.mat']);
+    mnist(:,(idx-1)*(numPerClass)+1:(idx-1)*(numPerClass)+numPerClass) = D(1:numPerClass,:)';
+end
+mnist = mnist ./ 255;
+
+target_template = eye(10);
+batchtargets = repmat(target_template,1,1,5000);
+
+batchdata = nan(10,784,5000);
+for idx=1:5000
+    for idx2=1:10
+        batchdata(idx2,:,idx) = mnist(:,(idx2-1)*5000+idx);
+    end
+    %shuff = randperm(10);
+    %batchdata = batchdata(shuff,:,:);
+    %batchtargets = batchtargets(shuff,:,:);
+end
+
+%%%%%%%%% set up test batches from hinton code %%%%%%%%%%%%%%%%%%
+clear digitdata targets;
+
+digitdata=[];
+targets=[];
+load test0; digitdata = [digitdata; D]; targets = [targets; repmat([1 0 0 0 0 0 0 0 0 0], size(D,1), 1)]; 
+load test1; digitdata = [digitdata; D]; targets = [targets; repmat([0 1 0 0 0 0 0 0 0 0], size(D,1), 1)]; 
+load test2; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 1 0 0 0 0 0 0 0], size(D,1), 1)];
+load test3; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 1 0 0 0 0 0 0], size(D,1), 1)];
+load test4; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 1 0 0 0 0 0], size(D,1), 1)];
+load test5; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 0 1 0 0 0 0], size(D,1), 1)];
+load test6; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 0 0 1 0 0 0], size(D,1), 1)];
+load test7; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 0 0 0 1 0 0], size(D,1), 1)];
+load test8; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 0 0 0 0 1 0], size(D,1), 1)];
+load test9; digitdata = [digitdata; D]; targets = [targets; repmat([0 0 0 0 0 0 0 0 0 1], size(D,1), 1)];
+digitdata = digitdata/255;
+
+totnum=size(digitdata,1);
+fprintf(1, 'Size of the test dataset= %5d \n', totnum);
+
+rand('state',0); %so we know the permutation of the training data
+randomorder=randperm(totnum);
+
+numbatches=totnum/100;
+numdims  =  size(digitdata,2);
+batchsize = 100;
+testbatchdata = zeros(batchsize, numdims, numbatches);
+testbatchtargets = zeros(batchsize, 10, numbatches);
+
+for b=1:numbatches
+  testbatchdata(:,:,b) = digitdata(randomorder(1+(b-1)*batchsize:b*batchsize), :);
+  testbatchtargets(:,:,b) = targets(randomorder(1+(b-1)*batchsize:b*batchsize), :);
+end;
+clear digitdata targets;
+
+
+%%% Reset random seeds 
+rand('state',sum(100*clock)); 
+randn('state',sum(100*clock)); 
